24d36969a81510323aec7d27aeb09a6a53f5f8b9
HakkanR <mesajtelecom@hotmail.com>
2021-06-27 00:41:40 +0300

    Update SerialDisplay.ino

--- examples/SerialDisplay/SerialDisplay.ino
+++ examples/SerialDisplay/SerialDisplay.ino
@@ -9,7 +9,7 @@
 #include <Wire.h> 
 #include <LiquidCrystal_I2cTUR.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2cTUR lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 void setup()
 {
